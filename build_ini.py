0e0c834970a09862de30e11739f8b30cae9d96bd
ereklemik <ereklemikiashvili@gmail.com>
2023-02-20 14:42:59 +0400

    Avoid DuplicateSectionError by checking if the section already exists before adding it

--- build_ini.py
+++ build_ini.py
@@ -2,23 +2,23 @@ from configparser import ConfigParser
 config = ConfigParser()
 file = 'applicant.ini'
 config.read(file)
-
-config.add_section('Applicant')
-
+if not config.has_section('Applicant'):
+    config.add_section('Applicant')
 section = config['Applicant']
 section['name'] = 'John'
 section['surname'] = 'Doe'
 section['position'] = 'DevOps Engineer'
 
-config.add_section('Skills')
+if not config.has_section('Skills'):
+    config.add_section('Skills')
 section = config['Skills']
-
 section['Cloud'] = 'AWS'
 section['Infrastructure'] = 'Terraform'
 section['Automation'] = 'Python'
 section['Monitoring'] = 'Prometheus'
 
-config.add_section('Education')
+if not config.has_section('Education'):
+    config.add_section('Education')
 section = config['Education']
 section['University'] = 'Harvard'
 section['Faculty'] = 'Information Systems'
@@ -31,6 +31,8 @@ additional_info = {
     'Address': 'Tbilisi',
 }
 
-config['AdditionalInfo'] = additional_info
+if not config.has_section('AdditionalInfo'):
+    config['AdditionalInfo'] = additional_info
+
 with open(file, 'w') as f:
     config.write(f)
